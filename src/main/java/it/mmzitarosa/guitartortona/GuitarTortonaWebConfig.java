2ef2f4470ca53fa38ba9478861bd047bbf767b3c
Matteo Zitarosa <matteomzitarosa@gmail.com>
2025-11-16 17:13:01 +0100

    fix: cors allow PATHC method

--- src/main/java/it/mmzitarosa/guitartortona/GuitarTortonaWebConfig.java
+++ src/main/java/it/mmzitarosa/guitartortona/GuitarTortonaWebConfig.java
@@ -15,7 +15,7 @@ public class GuitarTortonaWebConfig {
 				registry.addMapping("/**")
 //						.allowedOrigins("http://localhost:5173") // o la porta del tuo Vue
 						.allowedOrigins("*") // o la porta del tuo Vue
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 						.allowedHeaders("*");
 			}
 		};
